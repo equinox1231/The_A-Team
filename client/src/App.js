9a82a0b4e642163dc29f0aa6d86ab3dddc8b2149
Osama Ahmed Tahir <osama.Tahir@ibm.com>
2020-11-25 20:34:25 +0500

    Header Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Headers from './components/header'
+import HomePage from './components/HomePage'
+import Favicon from 'react-favicon';
+import { Switch, Route } from 'react-router-dom'
+//import reducers from './reducers'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+ }
+
+render() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+      
+        <Favicon url="" />
+        <Headers />
+          <Switch>
+           
+            <Route path="/" exact component={HomePage} />
+            
+        </Switch>
+      </div>
   );
+}
 }
 
 export default App;
