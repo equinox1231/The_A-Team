53d2711779286bb33c896a5431695224eea098ee
Muhammad Hamza <muha.hamza22@gmail.com>
2020-12-06 21:59:11 +0500

    run error

---
+++ client/src/views/signIn/signIn.jsx
@@ -0,0 +1,24 @@
+
+import React, { Component } from 'react';
+import Link from 'react';
+import { connect } from 'react-redux';
+// import * as actions from '../../actions';
+
+class signIn extends Component {
+
+  render() {
+    return (
+    
+      <div>
+          <div className="container">
+
+          <h3>Thank you for visiting LiveUp!!</h3>
+         
+          </div>
+      </div>
+
+    )
+  }
+}
+
+export default signIn;
