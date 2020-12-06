d62af104150bc516d6688f57f542f584ee048313
Osama Ahmed Tahir <osama.Tahir@ibm.com>
2020-12-06 20:53:23 +0500

    React Fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Headers from './components/header'
-import HomePage from './components/HomePage'
-import Favicon from 'react-favicon';
-<<<<<<< HEAD
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './reducers'
-//import Signin from './views/signin/signin'
-import Signup from './views/signup/signup'
-import Signout from './views/signout/signout'
 
-const store = createStore(
-  reducers,
-  {
-    auth: { authenticated: localStorage.getItem('token') }
-  },
-  applyMiddleware(reduxThunk)
-);
-=======
-import { BrowserRouter , Switch, Route, } from 'react-router-dom'
-//import reducers from './reducers'
->>>>>>> b27b320c530bf41ad22eae055be78b36f4f757ba
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-
-<<<<<<< HEAD
-    return (
-      <Provider store={store}>
-        <div className="App">
-          <Favicon url="" />
-          <BrowserRouter>
-            <Headers />
-            <Switch>
-              <Route path="/signup" component={Signup} />
-              <Route path="/signout" component={Signout} />
-              <Route path="/" exact component={HomePage} />
-            </Switch>
-          </BrowserRouter>
-        </div>
-      </Provider>
-    );
-  }
-=======
-      <div className="App">
-      
-        <Favicon url="" />
-        <Headers />
-          <BrowserRouter>
-            <Switch>
-            
-              <Route path="/" exact component={HomePage} />
-              
-            </Switch>
-        </BrowserRouter>
-      </div>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-}
->>>>>>> b27b320c530bf41ad22eae055be78b36f4f757ba
 }
 
 export default App;
